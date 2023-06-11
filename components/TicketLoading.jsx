b27ac1a875f064186ae62e530c5c251ca3c88dc1
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-11 12:22:56 +0700

    add component TicketLoading

---
+++ components/TicketLoading.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+const TicketLoading = () => {
+  return (
+    <div className="ticket-loading justify-center w-full">
+              <h1 className="font-medium text-sm leading-5 text-[#8A8A8A] text-center">Mencari penerbangan terbaik...</h1>
+              <div className="flex justify-center items-center">
+                <Image
+                className="mt-8"
+                src="/Loading.svg"
+                width={238}
+                height={100}
+                alt=""
+              />
+              </div>
+              
+            </div>
+  );
+};
+
+export default TicketLoading

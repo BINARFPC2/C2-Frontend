d3f32be7528db63b8274287b2c3d23bda18a5846
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-15 13:56:49 +0700

    add NavbarLogin

---
+++ components/NavbarLogin.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { FiSearch, FiLogIn } from "react-icons/fi";
+import Logo from "@/assets/images/logo-tiketku.svg";
+import IconList from "@/assets/fi_list.svg";
+import IconBell from "@/assets/icon_bell.svg";
+import IconUser from "@/assets/icon_user.svg";
+
+
+const NavbarLogin = () => {
+  return (
+    <div className="h-full max-w-full">
+      <div className="flex items-center justify-between w-full h-20 px-32 bg-white drop-shadow-md">
+        <div className="flex items-center w-1/2">
+          <Image
+            src={Logo}
+            width={78}
+            height={33}
+            alt="Picture of the author"
+          />
+        </div>
+
+        <div className="flex gap-x-6 ml-96">
+          <a href="#"><Image 
+          className="icon-list" 
+          src={IconList}
+          width={24} 
+          height={24} 
+          alt="" 
+          /></a>
+
+          <a href="#"><Image 
+          className="icon-list" 
+          src={IconBell}
+          width={24} 
+          height={24} 
+          alt="" 
+          /></a>
+
+          <a href="#"><Image 
+          className="icon-list" 
+          src={IconUser}
+          width={24} 
+          height={24} 
+          alt="" 
+          /></a>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarLogin;

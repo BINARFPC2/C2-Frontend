f7e032bf944fcd13039d5543bec8f17bf4548d53
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-16 19:48:48 +0700

    add riwayat, notif and user page

--- components/NavbarLogin.jsx
+++ components/NavbarLogin.jsx
@@ -11,16 +11,17 @@ const NavbarLogin = () => {
     <div className="h-full max-w-full">
       <div className="flex items-center justify-between w-full h-20 px-32 bg-white drop-shadow-md">
         <div className="flex items-center w-1/2">
+          <a href="/">
           <Image
             src={Logo}
             width={78}
             height={33}
             alt="Picture of the author"
-          />
+          /></a>
         </div>
 
         <div className="flex gap-x-6 ml-96">
-          <a href="#"><Image 
+          <a href="/riwayat"><Image 
           className="icon-list" 
           src={IconList}
           width={24} 
@@ -28,7 +29,7 @@ const NavbarLogin = () => {
           alt="" 
           /></a>
 
-          <a href="#"><Image 
+          <a href="/notif"><Image 
           className="icon-list" 
           src={IconBell}
           width={24} 
@@ -36,7 +37,7 @@ const NavbarLogin = () => {
           alt="" 
           /></a>
 
-          <a href="#"><Image 
+          <a href="/user"><Image 
           className="icon-list" 
           src={IconUser}
           width={24} 

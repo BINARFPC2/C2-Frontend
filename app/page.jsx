0e90c2a960324c6fd1e4024b1cbe32e7a2a029b3
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-23 04:06:31 +0700

    refactor beranda

--- app/page.jsx
+++ app/page.jsx
@@ -23,14 +23,14 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      
+
       <div className="h-screen md:max-w-full">
         <div className="relative flex items-center justify-center sm:h-1/2">
-        <div className="flex md:w-full h-[150px]">
-          <div className="hidden w-1/2 bg-[#B792D9] md:block"></div>
-          <div className="hidden w-1/2 bg-[#E2D4F0] md:block"></div>
-        </div>
-          
+          <div className="flex md:w-full h-[150px]">
+            <div className="hidden w-1/2 bg-[#B792D9] md:block"></div>
+            <div className="hidden w-1/2 bg-[#E2D4F0] md:block"></div>
+          </div>
+
           <div className="hidden absolute w-[80%] min-h-56 justify-end md:block ">
             <Image
               src={HeroImage}
@@ -45,36 +45,36 @@ export default function Home() {
         </div>
         <div className="container md:w-full min-h-fit flex flex-col">
           <h3 className="font-bold mb-4 sm:mx-32 px-2 text-left">Destinasi Favorit</h3>
-         
-<div className="md:w-full pb-10">
-  <div className="flex flex-wrap gap-4 justify-center">
-    {Categories.map((category) => (
-      <div className="w-auto sm:w-auto md:w-auto flex items-center bg-[#E2D4F0] hover:bg-bnr-primary py-2 px-4 md:py-2 md:px-6 rounded-xl hover:text-white" key={category.id}>
-        <FiSearch className="mr-2 md:mr-3" />
-      <span className="">{category.name}</span>
-      </div>
-    ))}
-  </div>
-</div>
 
           <div className="md:w-full pb-10">
-  <div className="flex flex-wrap gap-4 justify-center">
-    {Favorites.map((favorite) => (
-      <div className="w-auto sm:w-auto md:w-auto" key={favorite.id}>
-        <CardFavorite
-          from={favorite.from}
-          to={favorite.to}
-          airline={favorite.airline}
-          availableDate={favorite.availableDate}
-          price={favorite.price}
-        />
-      </div>
-    ))}
-  </div>
-</div>
+            <div className="flex flex-wrap gap-4 justify-center">
+              {Categories.map((category) => (
+                <div className="w-auto sm:w-auto md:w-auto flex items-center bg-[#E2D4F0] hover:bg-bnr-primary py-2 px-4 md:py-2 md:px-6 rounded-xl hover:text-white" key={category.id}>
+                  <FiSearch className="mr-2 md:mr-3" />
+                  <span className="">{category.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="md:w-full pb-10">
+            <div className="flex flex-wrap gap-4 justify-center">
+              {Favorites.map((favorite) => (
+                <div className="w-auto sm:w-auto md:w-auto" key={favorite.id}>
+                  <CardFavorite
+                    from={favorite.from}
+                    to={favorite.to}
+                    airline={favorite.airline}
+                    availableDate={favorite.availableDate}
+                    price={favorite.price}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
 
         </div>
-        
+
       </div>
     </>
   );

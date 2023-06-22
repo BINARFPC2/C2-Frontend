86feb5dcd1e35e37ef1c67377ed38b679e437120
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-23 04:02:19 +0700

    refactor beranda

--- app/page.jsx
+++ app/page.jsx
@@ -16,52 +16,65 @@ import NavbarLogin from "@/components/NavbarLogin";
 import { useRouter } from "next/navigation";
 import Loading from "./loading";
 
+
+
 export default function Home() {
 
   return (
     <>
       <Navbar />
-      <div className="h-screen max-w-full">
-        <div className="relative flex items-center justify-center h-1/2">
-          <div className="w-full h-32 bg-purple-400"></div>
-          <div className="absolute w-[80%] min-h-56 justify-end">
+      
+      <div className="h-screen md:max-w-full">
+        <div className="relative flex items-center justify-center sm:h-1/2">
+        <div className="flex md:w-full h-[150px]">
+          <div className="hidden w-1/2 bg-[#B792D9] md:block"></div>
+          <div className="hidden w-1/2 bg-[#E2D4F0] md:block"></div>
+        </div>
+          
+          <div className="hidden absolute w-[80%] min-h-56 justify-end md:block ">
             <Image
               src={HeroImage}
               alt="hero-image"
               className="z-0 w-full h-full" />
           </div>
         </div>
-        <div className="relative z-30 flex justify-center -mt-28">
+
+
+        <div className="relative z-30 flex justify-center sm:-mt-28  mb-10">
           <FormTicket />
         </div>
-        <div className="w-full min-h-fit px-72">
-          <h3 className="py-5 font-bold">Destinasi Favorit</h3>
-          <div className="flex gap-4">
-            {Categories.map((category) => (
-              <div
-                key={category.id}
-                className="inline-flex items-center bg-bnr-primary py-[14px] px-6 rounded-xl text-white"
-              >
-                <FiSearch className="mr-3" />
-                {category.name}
-              </div>
-            ))}
-          </div>
+        <div className="container md:w-full min-h-fit flex flex-col">
+          <h3 className="font-bold mb-4 sm:mx-32 px-2 text-left">Destinasi Favorit</h3>
+         
+<div className="md:w-full pb-10">
+  <div className="flex flex-wrap gap-4 justify-center">
+    {Categories.map((category) => (
+      <div className="w-auto sm:w-auto md:w-auto flex items-center bg-[#E2D4F0] hover:bg-bnr-primary py-2 px-4 md:py-2 md:px-6 rounded-xl hover:text-white" key={category.id}>
+        <FiSearch className="mr-2 md:mr-3" />
+      <span className="">{category.name}</span>
+      </div>
+    ))}
+  </div>
+</div>
+
+          <div className="md:w-full pb-10">
+  <div className="flex flex-wrap gap-4 justify-center">
+    {Favorites.map((favorite) => (
+      <div className="w-auto sm:w-auto md:w-auto" key={favorite.id}>
+        <CardFavorite
+          from={favorite.from}
+          to={favorite.to}
+          airline={favorite.airline}
+          availableDate={favorite.availableDate}
+          price={favorite.price}
+        />
+      </div>
+    ))}
+  </div>
+</div>
+
         </div>
-        <div className="w-full pb-10 px-72">
-          <h3 className="py-5 font-bold">Destinasi Favorit</h3>
-          <div className="flex gap-4">
-            {Favorites.map((favorite) => (
-              <CardFavorite
-                key={favorite.id}
-                from={favorite.from}
-                to={favorite.to}
-                airline={favorite.airline}
-                availableDate={favorite.availableDate}
-                price={favorite.price} />
-            ))}
-          </div>
-        </div>
+        
       </div>
     </>
   );

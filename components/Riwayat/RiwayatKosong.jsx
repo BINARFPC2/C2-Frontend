dd0b621070940a7f9cc6a31a9c989214e4f356a4
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-07-05 17:29:00 +0700

    fix error build

--- components/Riwayat/RiwayatKosong.jsx
+++ components/Riwayat/RiwayatKosong.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import Kosong from "@/assets/riwayat-kosong.svg";
 
@@ -5,20 +6,20 @@ import Kosong from "@/assets/riwayat-kosong.svg";
 const RiwayatKosong = () => {
   return (
     <>
-      <div className="riwayat-kosong mx-auto mt-16">
+      <div className="mx-auto mt-16 riwayat-kosong">
 
-          <Image
-            className="mx-auto"
-            src={Kosong}
-            width={204}
-            height={208}
-            alt=""
-          />
+        <Image
+          className="mx-auto"
+          src={Kosong}
+          width={204}
+          height={208}
+          alt=""
+        />
 
 
-        <div className="content mx-auto text-center mt-5">
+        <div className="mx-auto mt-5 text-center content">
           <div className="font-medium text-sm leading-5 text-[#7126B5]">Oops! No order history found!</div>
-          <div className="font-medium text-sm leading-5">You haven't made any flight bookings yet</div>
+          <div className="text-sm font-medium leading-5">You haven't made any flight bookings yet</div>
           <div className="mt-9">
             <button className="font-medium text-base leading-6 text-white w-[347px] h-[48px] bg-[#7126B5] rounded-xl">Search for Flights</button>
           </div>
